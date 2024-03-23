52d5b2c06b2089570d65faa7e653cb21a051c5e6
lmoo-m <fikrigamerz01@gmail.com>
2024-03-23 17:21:23 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="">
+            <h1 className="text-5xl">Supercluster</h1>
+        </div>
+    );
+}
+
+export default App;
